c52f7f1dcaeaba9c3e9a858e874883b6aa8a31c9
Egor <egor0288354@yandex.ru>
2022-10-27 18:42:35 +0300

    Add Genre & Film models, register 'em on admin page

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,7 @@
+import imp
 from django.contrib import admin
+from .models import Genre, Film
 
 # Register your models here.
+admin.site.register(Genre)
+admin.site.register(Film)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,26 @@
+from email.policy import default
+from operator import mod
+from pyexpat import model
+from statistics import mode
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Genre(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.title
+
+class Film(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    year = models.IntegerField()
+    logo_path = models.CharField(max_length=100)
+    genres = models.ManyToManyField(Genre)
+    kinopoisk_id = models.IntegerField(default=0)
+    create_date = models.DateTimeField('date created', default=timezone.now)
+
+    def __str__(self) -> str:
+        return self.title
