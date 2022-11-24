51e6365af685d83968af8c50707ee7a3ba35214d
Egor <egor0288354@yandex.ru>
2022-11-24 14:07:45 +0300

    Register category model

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,8 @@
 import imp
 from django.contrib import admin
-from .models import Genre, Film
+from .models import Genre, Film, Category
 
 # Register your models here.
 admin.site.register(Genre)
 admin.site.register(Film)
+admin.site.register(Category)
