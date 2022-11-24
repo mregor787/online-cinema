c33d6ad35c260bfdb536fa7ab69b99dde1918c4d
Egor <egor0288354@yandex.ru>
2022-11-24 13:54:07 +0300

    Add category model

--- app/models.py
+++ app/models.py
@@ -13,11 +13,18 @@ class Genre(models.Model):
     def __str__(self) -> str:
         return self.title
 
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.title
+
 class Film(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
     year = models.IntegerField()
     logo_path = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
     genres = models.ManyToManyField(Genre)
     kinopoisk_id = models.IntegerField(default=0)
     create_date = models.DateTimeField('date created', default=timezone.now)
