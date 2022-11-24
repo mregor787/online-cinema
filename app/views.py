594e41e3fb2b585cbdd7d2d761fb7652b500f730
Egor <egor0288354@yandex.ru>
2022-11-24 14:37:27 +0300

    Add index test template & test views

--- app/views.py
+++ app/views.py
@@ -1,8 +1,14 @@
-from http.client import HTTPResponse
-import imp
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Film
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the app index.")
+    films_list = Film.objects.order_by('title')[:5]
+    context = {'films_list': films_list}
+    return render(request, 'app/index.html', context)
+
+def detail(request, film_id):
+    title = Film.objects.filter(id=film_id)[0].title
+    return HttpResponse("Это страница фильма под названием: %s" % title)
