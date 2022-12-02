778ef908b3d3fb4227debe184d372c8cbc778200
Egor <egor0288354@yandex.ru>
2022-12-02 14:04:19 +0300

    Refactor index view and base & index templates

--- app/views.py
+++ app/views.py
@@ -8,18 +8,14 @@ from .models import Film, Category
 
 # Create your views here.
 def index(request):
-#    films_list = Film.objects.order_by('title')[:5]
-#   context = {'films_list': films_list}
-#    return render(request, 'app/index.html', context)
     category_list = []
-    for category in Category.objects.all():
-        category_list.append((category, translit(category.title, "ru", reversed=True))) 
-    slider_dict = {}
-    for category in category_list:
-        films = Film.objects.filter(category=category[0].id)
-        if films.count():
-            slider_dict[category[0].title] = films[randint(1, films.count()) - 1]
-    context = {'slider_dict': slider_dict, 'category_list': category_list}
+    for i, category in enumerate(Category.objects.all()):
+        category_list.append({
+            'object': category, 
+            'translit': translit(category.title, "ru", reversed=True),
+            'image_path': f'{i + 2}.jpg'
+        })
+    context = {'category_list': category_list}
     return render(request, 'app/index.html', context)
 
 def detail(request, film_id):
