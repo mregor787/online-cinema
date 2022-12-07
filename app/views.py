88abba54a812ed8e521f8444ac9b8d30ddbffcc9
Egor <egor0288354@yandex.ru>
2022-12-07 19:12:06 +0300

    Add base_context func in views.py

--- app/views.py
+++ app/views.py
@@ -2,27 +2,27 @@ from django.shortcuts import render
 
 from .models import Film, Category
 
-# Create your views here.
-def index(request):
-    categories = []
-    for i, category in enumerate(Category.objects.all()):
-        categories.append({
-            'object': category,
-            'title': category.get_plural().capitalize(),
-            'image_path': f'{i + 2}.jpg'
-        })
-    context = {'categories': categories}
-    return render(request, 'app/index.html', context)
-
-def category(request, category_id):
+def base_context():
     categories = []
     for category in Category.objects.all():
         categories.append({
             'object': category,
             'title': category.get_plural().capitalize()
         })
+    context = {'categories': categories}
+    return context
+
+def index(request):
+    context = base_context()
+    for i in range(len(context['categories'])):
+        context['categories'][i]['image_path'] = f'{i + 2}.jpg'
+    return render(request, 'app/index.html', context)
+
+def category(request, category_id):
+    context = base_context()
     category = {'object': Category.objects.filter(id=category_id)[0]}
     category['title'] = category['object'].get_plural().capitalize()
     films = Film.objects.filter(category=category['object'].id).order_by('create_date')
-    context = {'films': films, 'category': category, 'categories': categories}
+    context['films'] = films
+    context['category'] = category
     return render(request, 'app/category.html', context)
